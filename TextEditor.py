9970049ee857b3760515043e7d5900a097846c75
Tyler Ishikawa <tyi3@cornell.edu>
2015-12-27 03:32:40 -0500

    add menubar

--- TextEditor.py
+++ TextEditor.py
@@ -7,6 +7,18 @@ class MyFrame(wx.Frame):
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(200,100))
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar()
+
+        filemenu = wx.Menu()
+
+        filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
+        
+        # Creating the menubar.
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu, "&File")
+        self.SetMenuBar(menuBar)
         self.Show(True)
 
 app = wx.App(False)
