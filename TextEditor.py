2c20de44480714cb81f86a62314cb900661f8a50
Tyler Ishikawa <tyi3@cornell.edu>
2016-01-04 19:10:23 -0500

    adds event handlers and Open File feature

--- TextEditor.py
+++ TextEditor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import wx
+import os
 
 class MyFrame(wx.Frame):
     """ We simply derive a new class of Frame. """
@@ -9,18 +10,46 @@ class MyFrame(wx.Frame):
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         self.CreateStatusBar()
 
+        # Setting up the menu
         filemenu = wx.Menu()
 
-        filemenu.Append(wx.ID_ABOUT, "About", " Information about this program")
-        filemenu.AppendSeparator()
-        filemenu.Append(wx.ID_EXIT, "Exit", " Terminate the program")
-        
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "About", " Information about this program")
+        menuExit = filemenu.Append(wx.ID_EXIT, "Exit", " Terminate the program")
+        menuOpen = filemenu.Append(wx.ID_OPEN, "Open", " Open a file")
+
         # Creating the menubar.
         menuBar = wx.MenuBar()
         menuBar.Append(filemenu, "File")
         self.SetMenuBar(menuBar)
+
+        # Set events
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+        #filemenu.AppendSeparator()
+        
         self.Show(True)
 
+    def OnAbout(self, event):
+        dlg = wx.MessageDialog(self, "A small text editor", "About sample editor", wx.OK)
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    def OnExit(self, event):
+        self.Close(True)
+
+    def OnOpen(self, event):
+        """ Open a file """
+        self.dirname = ''
+        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            f = open(os.path.join(self.dirname, self.filename), 'r')
+            self.control.SetValue(f.read())
+            f.close()
+        dlg.Destroy()
+
 app = wx.App(False)
-frame = MyFrame(None, 'Small editor')
+frame = MyFrame(None, 'Sample editor')
 app.MainLoop()
